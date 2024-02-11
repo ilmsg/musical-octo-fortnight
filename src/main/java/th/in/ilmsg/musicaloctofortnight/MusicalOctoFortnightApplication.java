5de26601e84af42e1778862954c99eadbbc5fa35
eak netpanya <eak.netpanya@gmail.com>
2024-02-11 08:23:17 +0700

    [init] project

---
+++ src/main/java/th/in/ilmsg/musicaloctofortnight/MusicalOctoFortnightApplication.java
@@ -0,0 +1,13 @@
+package th.in.ilmsg.musicaloctofortnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalOctoFortnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalOctoFortnightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/in/ilmsg/musicaloctofortnight/MusicalOctoFortnightApplicationTests.java
@@ -0,0 +1,13 @@
+package th.in.ilmsg.musicaloctofortnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalOctoFortnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
